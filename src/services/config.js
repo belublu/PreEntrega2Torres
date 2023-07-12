b1e29ce0f8403ee81fcb9b1c6b592f1625b061fb
Belen Torres <belentorress@gmail.com>
2023-07-12 19:17:33 -0300

    Instalación de Firebase

---
+++ src/services/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDfmRdKaGpcL0t5o62Ks5FQmtXZoQXx6mk",
+    authDomain: "scentopia-efeac.firebaseapp.com",
+    projectId: "scentopia-efeac",
+    storageBucket: "scentopia-efeac.appspot.com",
+    messagingSenderId: "193430500278",
+    appId: "1:193430500278:web:7cb5570edd47bace658717"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
